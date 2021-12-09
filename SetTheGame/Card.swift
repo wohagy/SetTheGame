99b20be7270268682d9be445f3de88dd89b2f14f
Emelyanov Roman <wohagy@gmail.com>
2021-12-09 22:21:04 +0300

    made struct to Set's card

--- SetTheGame/Card.swift
+++ SetTheGame/Card.swift
@@ -7,6 +7,43 @@
 
 import Foundation
 
-struct Card {
-
+struct Card: Equatable, CustomStringConvertible {
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return ((lhs.number==rhs.number) &&
+                (lhs.color==rhs.color) &&
+                (lhs.fill==rhs.fill) &&
+                (lhs.shape)==(rhs.shape)
+        )
+    }
+    
+    
+    var description: String
+    
+    let number: Variant
+    let color: Variant
+    let shape: Variant
+    let fill: Variant
+    
+    enum Variant: Int, CustomStringConvertible {
+        
+        case v1 = 1
+        case v2
+        case v3
+        
+        static var all: [Variant] {return [.v1,.v2,.v3]}
+        var description: String {return String(self.rawValue)}
+        var idx: Int {return (self.rawValue - 1)}
+    }
+    
+    static func isSet(cards: [Card]) -> Bool {
+        guard cards.count == 3 else {return false}
+        let sum = [
+            cards.reduce(0, {$0 + $1.color.rawValue}),
+            cards.reduce(0, {$0 + $1.fill.rawValue}),
+            cards.reduce(0, {$0 + $1.shape.rawValue}),
+            cards.reduce(0, {$0 + $1.number.rawValue})
+        ]
+        return sum.reduce(true, { $0 && ($1 % 3 == 0) })
+    }
 }
+
--- SetTheGame/ViewController.swift
+++ SetTheGame/ViewController.swift
@@ -18,8 +18,10 @@ class ViewController: UIViewController {
     
     private func updateViewFromModel() {
         for index in cards.indices{
+            let button = cards[index]
+            button.setTitle(String(index), for: UIControl.State.normal)
             if index < 12 {
-                cards[index].setTitle(String(index), for: UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         }
     }
 }
